4981874f6746640e513533e072cefa989a2d15b3
Asya Zlatkova <asazlatkova@gmail.com>
2026-03-24 22:31:12 +0200

    Added IBookService

--- demo/Services/BookService.cs
+++ demo/Services/BookService.cs
@@ -1,7 +1,33 @@
-﻿namespace catalog.Services
-{
+﻿using catalog.Entities;
+
+namespace catalog.Services;
+
     public class BookService
     {
-       
+        private static List<Book> books = new List<Book>();
+
+        public List<Book> GetAll()
+        {
+            return books;
+        }
+
+        public Book? GetById(int id)
+        {
+            return books.FirstOrDefault(b => b.Id == id);
+        }
+
+        public void Add(Book book)
+        {
+            book.Id = books.Count + 1;
+            books.Add(book);
+        }
+
+        public void Delete(int id)
+        {
+            var book = GetById(id);
+            if (book != null)
+            {
+                books.Remove(book);
+            }
+        }
     }
-}
--- demo/Services/IBookService.cs
+++ demo/Services/IBookService.cs
@@ -1,6 +1,11 @@
-﻿namespace catalog.Services
-{
-    public class IBookService
+﻿ using catalog.Entities;
+
+namespace catalog.Services;
+
+    public interface IBookService
     {
+        List<Book> GetAll();
+        Book? GetById(int id);
+        void Add(Book book);
+        void Delete(int id);
     }
-}
