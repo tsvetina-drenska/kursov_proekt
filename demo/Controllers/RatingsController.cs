de346189d6fd7decd640ef9a26e02d89a84502b7
Asya Zlatkova <asazlatkova@gmail.com>
2026-03-24 21:18:00 +0200

    Added BookService

--- demo/Controllers/RatingsController.cs
+++ demo/Controllers/RatingsController.cs
@@ -1,12 +1,1 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace catalog.Controllers
-{
-    public class RatingsController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
-}
+﻿  
