51b289c965116f4b92a345b49ff2651366a2e961
Asya Zlatkova <asazlatkova@gmail.com>
2026-03-25 20:08:39 +0200

    added MovieService

--- demo/Services/MovieService.cs
+++ demo/Services/MovieService.cs
@@ -1,6 +1,33 @@
-﻿namespace catalog.Services
-{
-    public class MovieService
+﻿using catalog.Entities;
+
+namespace catalog.Services;
+
+    public class MovieService : IMovieService
     {
+        private static List<Movie> movies = new List<Movie>();
+
+        public List<Movie> GetAll()
+        {
+            return movies;
+        }
+
+        public Movie? GetById(int id)
+        {
+            return movies.FirstOrDefault(m => m.Id == id);
+        }
+
+        public void Add(Movie movie)
+        {
+            movie.Id = movies.Count + 1;
+            movies.Add(movie);
+        }
+
+        public void Delete(int id)
+        {
+            var movie = GetById(id);
+            if (movie != null)
+            {
+                movies.Remove(movie);
+            }
+        }
     }
-}
