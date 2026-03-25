f4a6b885bb836dd042441dc4498edb8d5deace20
tsvetina-drenska <drenska.tsvetina@gmail.com>
2026-03-25 18:14:34 +0200

    Adding book and movie controller

--- demo/Controllers/MovieController.cs
+++ demo/Controllers/MovieController.cs
@@ -1,6 +1,87 @@
-﻿namespace catalog.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using catalog.Entities;
+using catalog.Services;
+
+
+namespace catalog.Controllers
 {
-    public class MovieController
+    public class MovieController : Controller
     {
+        private readonly MovieService _movieService;
+        public MovieController()
+        {
+            _movieService = new MovieService();
+        }
+        
+        // GET: /Movie
+        public IActionResult Index()
+        {
+            var movies = _movieService.GetAll();
+            return View(movies);
+        }
+        
+        // GET: /Movie/Details/ID
+        public IActionResult Details(int id)
+        {
+            var movie = _movieService.GetById(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+
+        }
+
+        // GET: /Movie/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        // POST: /Movie/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _movieService.Add(movie);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(movie);
+
+        }
+
+        // GET: /Movie/Delete/ID
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _movieService.Update(movie);
+                return RedirectToAction(nameof(Index));
+
+            }
+
+            return View(movie);
+        }
+
+        // POST: /Movie/Delete/ID
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _movieService.Delete(id);
+            return RedirectToAction(nameof(Index));
+
+        }
+
     }
+
 }
+
+
