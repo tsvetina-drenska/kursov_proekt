8d8b85987917d82c5c991a49170d89a9076fcb0a
Asya Zlatkova <asazlatkova@gmail.com>
2026-03-25 19:55:10 +0200

    added IMovieService

--- demo/Services/IMovieService.cs
+++ demo/Services/IMovieService.cs
@@ -1,6 +1,13 @@
-﻿namespace catalog.Services
-{
-    public class IMovieService
+﻿ using catalog.Entities;
+
+namespace catalog.Services;
+
+    public interface IMovieService
     {
+        List<Movie> GetAll();
+        Movie? GetById(int id);
+        void Add(Movie movie);
+        void Delete(int id);
     }
-}
+
+
