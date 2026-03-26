ba97c2d8a7222d5e66cc2c35aea304fe1fe6b17d
Asya Zlatkova <asazlatkova@gmail.com>
2026-03-26 23:55:51 +0200

    Added IAuthService

--- demo/Services/IAuthService.cs
+++ demo/Services/IAuthService.cs
@@ -1,6 +1,12 @@
-﻿namespace catalog.Services
-{
-    public class IAuthService
+﻿using catalog.Entities;
+
+namespace catalog.Services
     {
+        // Интерфейс, който описва какво трябва да може AuthService
+        public interface IAuthService
+        {
+            User? Login(string username, string password); // Вход
+            void Register(User user); // Регистрация
+            User? GetByUsername(string username); // Търсене на потребител
+        }
     }
-}
