cbc1e72bb2c9f364eef4464940cd3dcd34f1ae2e
Asya Zlatkova <asazlatkova@gmail.com>
2026-03-26 21:42:57 +0200

    Added AuthService

--- demo/Services/AuthService.cs
+++ demo/Services/AuthService.cs
@@ -1,6 +1,35 @@
-﻿namespace catalog.Services
+﻿using catalog.Entities;
+
+namespace catalog.Services
 {
-    public class AuthService
+    // Сервиз за управление на потребители (логин/регистрация)
+    public class AuthService : IAuthService
     {
+        // Списък с потребители 
+        private static List<User> users = new List<User>();
+
+        // Метод за вход
+        public User? Login(string username, string password)
+        {
+            // Търси потребител със съвпадащо име и парола
+            return users.FirstOrDefault(u =>
+                u.Username == username && u.PasswordHash == password);
+        }
+
+        // Метод за регистрация
+        public void Register(User user)
+        {
+            // Генерираме ID
+            user.Id = users.Count + 1;
+
+            // Добавяме потребителя в списъка
+            users.Add(user);
+        }
+
+        // Връща потребител по username
+        public User? GetByUsername(string username)
+        {
+            return users.FirstOrDefault(u => u.Username == username);
+        }
     }
 }
