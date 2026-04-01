c31a9a82c068be23e78d70e1d3e94f153fda53c3
Asya Zlatkova <asazlatkova@gmail.com>
2026-04-01 22:54:16 +0300

    BookRepository and MovieRepository

--- demo/Repositories/MovieRepository.cs
+++ demo/Repositories/MovieRepository.cs
@@ -1,6 +1,54 @@
-﻿namespace catalog.Repositories
+﻿using System.Collections.Generic;
+using System.Linq;
+using catalog.Data;
+using catalog.Entities;
+
+namespace catalog.Repositories;
+
+public class MovieRepository
 {
-    public class Class
+    private readonly ApplicationDbContext _context;
+
+    // Конструктор
+    public MovieRepository(ApplicationDbContext context)
     {
+        _context = context;
+    }
+
+    // Връща всички филми
+    public List<Movie> GetAll()
+    {
+        return _context.Movies.ToList();
+    }
+
+    // Връща филм по id
+    public Movie GetById(int id)
+    {
+        return _context.Movies.FirstOrDefault(m => m.Id == id);
+    }
+
+    // Добавя нов филм
+    public void Add(Movie movie)
+    {
+        _context.Movies.Add(movie);
+        _context.SaveChanges();
+    }
+
+    // Обновява филм
+    public void Update(Movie movie)
+    {
+        _context.Movies.Update(movie);
+        _context.SaveChanges();
+    }
+
+    // Изтрива филм по id
+    public void Delete(int id)
+    {
+        var movie = _context.Movies.FirstOrDefault(m => m.Id == id);
+        if (movie != null)
+        {
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+        }
     }
 }
